5fbccbdc6b8d5c6beea7043a41194d7eecdb0531
Colkimi Mibey <141415279+Colkimi@users.noreply.github.com>
2025-04-24 12:32:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
 
 app.get("/api/notes", async (req, res) => {
   const notes = await prisma.note.findMany();
